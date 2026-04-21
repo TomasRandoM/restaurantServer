91b0a891ee1f6ae62a1b700244b8daec50b4a196
Tomás Rando <tomasrando@gmail.com>
2026-04-21 18:53:55 -0300

    Initialize project

---
+++ src/main/java/com/apkrew/staffManagementServer/StaffManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.apkrew.staffManagementServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffManagementServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apkrew/staffManagementServer/StaffManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apkrew.staffManagementServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
