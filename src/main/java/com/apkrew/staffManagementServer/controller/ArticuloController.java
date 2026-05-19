0a86bb78527067f21a5947cf7f222d72299246ef
JoacoR7 <joaquin.ruiz2012@gmail.com>
2026-05-19 16:11:21 -0300

    CRUD artículo

--- src/main/java/com/apkrew/staffManagementServer/controller/ArticuloController.java
+++ src/main/java/com/apkrew/staffManagementServer/controller/ArticuloController.java
@@ -1,6 +1,8 @@
 package com.apkrew.staffManagementServer.controller;
 
+import com.apkrew.staffManagementServer.domain.entity.Articulo;
 import com.apkrew.staffManagementServer.domain.entity.UnidadDeMedida;
+import com.apkrew.staffManagementServer.domain.service.ArticuloServiceImpl;
 import com.apkrew.staffManagementServer.domain.service.UnidadDeMedidaServiceImpl;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(path = "/api/v1/unidadDeMedida")
-public class UnidadDeMedidaController extends BaseControllerImpl<UnidadDeMedida, UnidadDeMedidaServiceImpl> {
-    public UnidadDeMedidaController(UnidadDeMedidaServiceImpl service) {
+@RequestMapping(path = "api/v1/articulo")
+public class ArticuloController extends BaseControllerImpl<Articulo, ArticuloServiceImpl> {
+    public ArticuloController(ArticuloServiceImpl service) {
         super(service);
     }
 }
